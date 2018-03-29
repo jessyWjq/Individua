41ba24e574c6a9d789c97b1cbac7e88e8c16db90
jessy <893269725@qq.com>
2018-03-29 16:55:41 +0800

    core

---
+++ Individua.coreCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Individua.coreCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
